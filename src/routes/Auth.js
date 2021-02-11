4a6399521d287ba12d2103da239dfaf4ef93185f
길재우 <6197@.>
2021-02-11 17:42:07 +0900

    Initialize project using Create React App

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,12 @@
 import React ,{useState} from 'react';
+import {authService} from "../fbase";
 
 
 const Auth = () =>{
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const [newAccount,setNewAccount] = useState('true');
+    const [error,setError] = useState("");
     const onChange = (event) =>{
         const{
             target: {name, value},
@@ -15,9 +18,25 @@ const Auth = () =>{
             setPassword(value);
         }
     }
-    const onSubmit = (event) =>{
+    const onSubmit = async (event) => {
+        //사이트 새로고침과 같은 기본행위가 일어나지 않도록 제약
         event.preventDefault();
-    }
+        try {
+            let data;
+            if (newAccount) {
+                const data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                )
+            } else {
+                const data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        }catch(error){
+            setError(error.message);
+        }
+    };
+
+    const toggleAccount = () => setNewAccount((prev)=> !prev);
     return(
         <div>
             <form onSubmit={onSubmit}>
@@ -25,8 +44,10 @@ const Auth = () =>{
                        required={true} value={email} onChange={onChange}/>
                 <input name="password" type={"password"} placeholder={"Password"}
                        required={true} value={password} onChange={onChange}/>
-                <input type={"submit"} value={"Log In"} />
+                <input type={"submit"} value={newAccount ? "Create Account" : "Log In"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "Sign In":"CreateAccount"}</span>
             <div>
                 <button>Continue with Google</button>
                 <button>Continue with GitHub</button>
